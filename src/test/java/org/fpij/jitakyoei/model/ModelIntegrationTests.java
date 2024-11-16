0b91fcb614490ef57391cdb2d13e27e49ad2cfe4
Luc Bonnet <lucbonnet10@gmail.com>
2024-11-16 16:38:46 -0300

    Correção do diretório de testes

--- src/test/java/org/fpij/jitakyoei/model/ModelIntegrationTests.java
+++ src/test/java/org/fpij/jitakyoei/model/ModelIntegrationTests.java
@@ -1,4 +1,4 @@
-package java.org.fpij.jitakyoei.model;
+package test.java.org.fpij.jitakyoei.model;
 
 import java.util.Date;
 
@@ -13,16 +13,16 @@ import main.java.org.fpij.jitakyoei.model.beans.Rg;
 import main.java.org.fpij.jitakyoei.util.DatabaseManager;
 
 public class ModelIntegrationTests {
-//	private static DAO<Aluno> alunoDao;
+	// private static DAO<Aluno> alunoDao;
 	private static Aluno aluno;
 	private static Entidade entidade;
 	private static Endereco endereco;
 	private static Filiado filiadoAluno;
 	private static Filiado filiadoProf;
 	private static Professor professor;
-	
+
 	@BeforeClass
-	public static void setUp(){
+	public static void setUp() {
 		DatabaseManager.setEnviroment(DatabaseManager.TEST);
 		filiadoAluno = new Filiado();
 		filiadoAluno.setNome("Aécio");
@@ -31,14 +31,14 @@ public class ModelIntegrationTests {
 		filiadoAluno.setDataCadastro(new Date());
 		filiadoAluno.setId(1332L);
 		filiadoAluno.setRg(new Rg("5026762-0", "SSP-PI"));
-		
+
 		endereco = new Endereco();
 		endereco.setBairro("Dirceu");
 		endereco.setCep("64078-213");
 		endereco.setCidade("Teresina");
 		endereco.setEstado("PI");
 		endereco.setRua("Rua Des. Berilo Mota");
-		
+
 		filiadoProf = new Filiado();
 		filiadoProf.setNome("Professor");
 		filiadoProf.setCpf("036.464.453-27");
@@ -46,21 +46,21 @@ public class ModelIntegrationTests {
 		filiadoProf.setDataCadastro(new Date());
 		filiadoProf.setId(3332L);
 		filiadoProf.setEndereco(endereco);
-		
+
 		professor = new Professor();
 		professor.setFiliado(filiadoProf);
-		
+
 		entidade = new Entidade();
 		entidade.setEndereco(endereco);
 		entidade.setNome("Academia 1");
 		entidade.setTelefone1("(086)1234-5432");
-		
+
 		aluno = new Aluno();
 		aluno.setFiliado(filiadoAluno);
 		aluno.setProfessor(professor);
 		aluno.setEntidade(entidade);
-		
-//		alunoDao = new DAOImpl<Aluno>(Aluno.class);
+
+		// alunoDao = new DAOImpl<Aluno>(Aluno.class);
 	}
-	
+
 }

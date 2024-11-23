36bd0b7d8d4c6336bf6ffb1a7d54626a692833a8
Luc Bonnet <lucbonnet10@gmail.com>
2024-11-23 18:27:45 -0300

    Criação do teste salvar professor

--- src/test/java/org/fpij/jitakyoei/model/dao/ProfessorDaoTest.java
+++ src/test/java/org/fpij/jitakyoei/model/dao/ProfessorDaoTest.java
@@ -1,5 +1,7 @@
 package test.java.org.fpij.jitakyoei.model.dao;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -7,11 +9,13 @@ import java.util.List;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import main.java.org.fpij.jitakyoei.model.beans.Aluno;
 import main.java.org.fpij.jitakyoei.model.beans.Endereco;
 import main.java.org.fpij.jitakyoei.model.beans.Entidade;
 import main.java.org.fpij.jitakyoei.model.beans.Filiado;
 import main.java.org.fpij.jitakyoei.model.beans.Professor;
 import main.java.org.fpij.jitakyoei.model.dao.DAO;
+import main.java.org.fpij.jitakyoei.model.dao.DAOImpl;
 import main.java.org.fpij.jitakyoei.util.DatabaseManager;
 
 public class ProfessorDaoTest {
@@ -57,5 +61,40 @@ public class ProfessorDaoTest {
     professor = new Professor();
     professor.setFiliado(f1);
     professor.setEntidades(entidades);
+
+    professorDao = new DAOImpl<Professor>(Professor.class);
+  }
+
+  public static void clearDatabase() {
+    List<Professor> all = professorDao.list();
+    for (Professor each : all) {
+      professorDao.delete(each);
+    }
+    assertEquals(0, professorDao.list().size());
+  }
+
+  @Test
+  public void salvarProfessorTest() {
+    clearDatabase();
+
+    assertEquals(professorDao.list().size(), 0);
+
+    professorDao.save(professor);
+
+    assertEquals(professorDao.list().size(), 1);
+
+    assertEquals("Matheus", professorDao.get(professor).getFiliado().getNome());
+    assertEquals("206.561.170-79", professorDao.get(professor).getFiliado().getCpf());
+
+    assertEquals("Dirceu", professorDao.get(professor).getFiliado().getEndereco().getBairro());
+    assertEquals("64078-213", professorDao.get(professor).getFiliado().getEndereco().getCep());
+    assertEquals("Teresina", professorDao.get(professor).getFiliado().getEndereco().getCidade());
+    assertEquals("PI", professorDao.get(professor).getFiliado().getEndereco().getEstado());
+    assertEquals("Rua Des. Berilo Mota", professorDao.get(professor).getFiliado().getEndereco().getRua());
+
+    assertEquals(2, professorDao.get(professor).getEntidades().size());
+
+    assertEquals("Academia 1", professorDao.get(professor).getEntidades().get(0).getNome());
+    assertEquals("Academia 2", professorDao.get(professor).getEntidades().get(1).getNome());
   }
 }

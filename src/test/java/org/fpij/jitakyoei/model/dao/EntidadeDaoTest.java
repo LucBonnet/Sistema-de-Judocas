1debd7cac7be32f0ba5a3ba6d579b32eacfb82d2
Luc Bonnet <lucbonnet10@gmail.com>
2024-11-24 01:20:27 -0300

    Criação do teste adicionar entidade

--- src/test/java/org/fpij/jitakyoei/model/dao/EntidadeDaoTest.java
+++ src/test/java/org/fpij/jitakyoei/model/dao/EntidadeDaoTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.util.List;
 
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import main.java.org.fpij.jitakyoei.model.beans.Aluno;
 import main.java.org.fpij.jitakyoei.model.beans.Endereco;
@@ -16,6 +17,7 @@ import main.java.org.fpij.jitakyoei.util.DatabaseManager;
 public class EntidadeDaoTest {
   private static DAO<Entidade> entidadeDao;
   private static Endereco endereco;
+  private static Entidade entidade;
 
   @BeforeClass
   public static void setUp() {
@@ -28,7 +30,7 @@ public class EntidadeDaoTest {
     endereco.setEstado("PI");
     endereco.setRua("Rua Des. Berilo Mota");
 
-    Entidade entidade = new Entidade();
+    entidade = new Entidade();
     entidade.setNome("Academia 1");
     entidade.setTelefone1("(88) 2802-1826");
     entidade.setTelefone2("(88) 98516-2167");
@@ -44,5 +46,25 @@ public class EntidadeDaoTest {
       entidadeDao.delete(each);
     }
     assertEquals(0, entidadeDao.list().size());
+  }
+
+  @Test
+  public void adicionarEntidadeTest() {
+    clearDatabase();
+
+    assertEquals(0, entidadeDao.list().size());
+
+    entidadeDao.save(entidade);
+
+    assertEquals(1, entidadeDao.list().size());
+    assertEquals("Academia 1", entidadeDao.get(entidade).getNome());
+    assertEquals("(88) 2802-1826", entidadeDao.get(entidade).getTelefone1());
+    assertEquals("(88) 98516-2167", entidadeDao.get(entidade).getTelefone2());
+    assertEquals("60.580.350/0001-04", entidadeDao.get(entidade).getCnpj());
+
+    assertEquals("Dirceu", entidadeDao.get(entidade).getEndereco().getBairro());
+    assertEquals("64078-213", entidadeDao.get(entidade).getEndereco().getCep());
+    assertEquals("PI", entidadeDao.get(entidade).getEndereco().getEstado());
+    assertEquals("Rua Des. Berilo Mota", entidadeDao.get(entidade).getEndereco().getRua());
   }
 }
